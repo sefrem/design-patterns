f943b95f14cefd5955463f59d6d728f00b08dad9
sefrem <fireweb2112@gmail.com>
2020-02-02 22:26:24 +0300

    observer pattern

---
+++ src/com/company/observer/Main.java
@@ -0,0 +1,17 @@
+package com.company.observer;
+
+public class Main {
+    public static void main(String[] args) {
+        var dataSource = new DataSource();
+        var sheet1 = new SpreadSheet(dataSource);
+        var sheet2 = new SpreadSheet(dataSource);
+        var chart = new Chart(dataSource);
+
+        dataSource.addObserver(sheet1);
+        dataSource.addObserver(sheet2);
+        dataSource.addObserver(chart);
+
+        dataSource.setValue(44);
+    }
+
+}
